b34a6bb682561b1ffe049b58ceec436c514847d4
a1 <a1@a1s-MacBook-Pro.local>
2023-03-22 04:31:43 -0500

    Import Scanner class and take in user input

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
+
+    Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
         
+        System.out.println("What is your name? ");
+        String name = in.nextLine();
+        System.out.println("Do you really expect me to believe that your name is " + name + "?");
+
     }
 }
