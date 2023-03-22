94c378b33fb57516c56ccadd8ec5c69ef4c48bb3
a1 <a1@a1s-MacBook-Pro.local>
2023-03-22 04:37:07 -0500

    Move Scanner instance inside of main, trim user input, close Scanner

--- Main.java
+++ Main.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    Scanner in = new Scanner(System.in);
-
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in); //instantiate scanner
         
-        System.out.println("What is your name? ");
-        String name = in.nextLine();
+        System.out.println("What is your name? "); //take in user input
+        String name = in.nextLine().trim();
         System.out.println("Do you really expect me to believe that your name is " + name + "?");
+
+        in.close(); //close Scanner
 
     }
 }
